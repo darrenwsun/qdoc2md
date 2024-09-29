9e5de57f6083e17db05bdc8dee13688998017df7
Darren Sun <darren.sun@darrensun.net>
2024-09-29 19:54:16 +0800

    Simplify code structure; standardize markdown layout

--- src/qdoc2md/main.py
+++ src/qdoc2md/main.py
@@ -1,16 +1,17 @@
-import sys
+import argparse
 
-from qdoc2md.generator import Generator
+from qdoc2md.generator import generate
 
 
 def qdoc2md() -> None:
     """
     Convert q documentation comments into Markdown documents.
     """
-    if len(sys.argv) != 2:
-        raise Exception('Expect one argument for q source directory')
-    generator = Generator()
-    generator.generate(src=sys.argv[1])
+    parser = argparse.ArgumentParser(description='Generate Markdown documents from q documentation comments')
+    parser.add_argument('-s', '--src', default='src', nargs='*', help='Source files or directories')
+    parser.add_argument('-t', '--target', default='docs', help='Output directory for generated docs')
+    args = parser.parse_args()
+    generate(args.src, args.target)
 
 
 if __name__ == "__main__":
--- src/qdoc2md/param.py
+++
@@ -1,5 +0,0 @@
-class Param(object):
-    def __init__(self, name, datatype, description):
-        self.name = name
-        self.datatype = datatype
-        self.description = description
